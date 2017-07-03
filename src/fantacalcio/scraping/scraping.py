abe39f3114c119fef9e92540da6b6e4c698d1e2c
zuk-8 <zuk-8@LAPTOP-MBCAT1HG.home>
2017-07-03 18:58:09 +0100

    Scraping codes, script, csv with data

---
+++ src/fantacalcio/scraping/scraping.py
@@ -0,0 +1,43 @@
+import pandas as pd
+import requests
+from bs4 import BeautifulSoup
+
+
+INDICES=['V', 'G', 'A', 'R', 'RS', 'AG', 'AM', 'ES', 'FV']
+
+def clean_table_cells(st):
+    st = st.replace('\r', '').replace('\n', '').replace('\t', '').replace(' ', '')
+    try:
+        return float(st)
+    except ValueError:
+        return pd.np.NaN
+
+def get_team_soups(page_votes, giornata):
+    # Get html data from page+giornata
+    page = page_votes+str(giornata)
+    r = requests.get(page)
+    data = r.text
+    # Navigate inside data using BS
+    soup = BeautifulSoup(data, "lxml")
+    soup1 = [sec for sec in soup.html.body.find_all('section') if sec.get('class', None)==['main-container']][0]
+    soup2 = [sec for sec in soup1.find_all('section') if sec.get('class', None)==['section-standard-row']][0]
+    soup3 = [sec for sec in soup2.find_all('div') if sec.get('class', None)==["MXXX-central-articles-main-column"]][0]
+    soup4 = [sec for sec in soup3.find_all('div') if
+                   sec.get('class', None)==['magicDayList', 'listView', 'magicDayListChkDay']][0]
+    return [sec for sec in soup4.find_all('div') if sec.get('class', None)==['singleRound']]
+
+def get_player_soup(team_soup):
+    team_list = [sec for sec in team_soup.find_all('div') if sec.get('class', None)==["magicTeamListContainer"]][0]
+    team_grades = [sec for sec in team_list.find_all('ul') if sec.get('class')==["magicTeamList"]][0]
+    team = [span.contents[0] for span in team_grades.li.div.find_all('span') if span.get('class', None)==["teamNameIn"]][0]
+    player_soups = team_grades.find_all('li')[1:]
+    return team, player_soups
+
+def get_player_stats(player_soup, indices=INDICES):
+    player_name = [span.string for span in player_soup.div.div.find_all('span') if span.get('class',None)==["playerNameIn"]][0]
+    role = [span.string for span in player_soup.div.find_all('span') if
+            span.get('class',None)==["playerRole", "show-for-small"]][0]
+    stats = pd.Series(data=[clean_table_cells(div.string)
+            for div in player_soup.find_all('div') if div.get('class', None)[0] == "inParameter"],
+      index=indices)
+    return player_name, role, stats
---
+++ src/fantacalcio/scripts/get_grades_from_gazzetta.py
@@ -0,0 +1,48 @@
+import os
+import getpass
+import pandas as pd
+
+from scraping import get_team_soups, get_player_soup, get_player_stats
+from fantacalcio.config_data.config_data import FOLDER_FANTA_INPUT
+
+
+# INPUTS
+
+# page_votes = "https://www.fantagazzetta.com/voti-serie-a/2016-17/"
+PAGES_GRADES = {'2014':"http://www.gazzetta.it/calcio/fantanews/voti/serie-a-2014-15/giornata-",
+                '2015':"http://www.gazzetta.it/calcio/fantanews/voti/serie-a-2015-16/giornata-",
+                '2016':"http://www.gazzetta.it/calcio/fantanews/voti/serie-a-2016-17/giornata-",}
+N_GIORNATE = 38
+INDICES=['V', 'G', 'A', 'R', 'RS', 'AG', 'AM', 'ES', 'FV']
+OUTPUT_FOLDER = FOLDER_FANTA_INPUT[getpass.getuser()]
+OUTPUT_FN = "fanta_grades.csv"
+
+
+# FUNCTIONS
+
+def main():
+    all_stats = pd.DataFrame()
+    for season,page_grades in PAGES_GRADES.items():
+        season_stats = pd.DataFrame()
+        for giornata in range(1,N_GIORNATE+1):
+            team_stats = pd.DataFrame()
+            for team_soup in get_team_soups(page_grades, giornata):
+                team, player_soups = get_player_soup(team_soup)
+                player_stats = pd.DataFrame()
+                for player_soup in player_soups:
+                    player_name, role, stats = get_player_stats(player_soup, indices=INDICES)
+                    ps = pd.DataFrame({player_name:stats}).T
+                    ps['Role'] = role
+                    player_stats = pd.concat([player_stats, ps])
+                player_stats = player_stats.reset_index().rename(columns={'index':'Player'})
+                player_stats['Team'] = team
+                team_stats = pd.concat([team_stats, player_stats])
+            team_stats['Giornata'] = str(giornata)
+            season_stats = pd.concat([season_stats, team_stats])
+        season_stats['Season'] = season
+        all_stats = pd.concat([all_stats, season_stats])
+    all_stats = all_stats.set_index(['Season', 'Giornata', 'Team', 'Player'])
+    all_stats.to_csv(os.path.join(OUTPUT_FOLDER, OUTPUT_FN))
+
+if __name__ == '__main__':
+    main()
