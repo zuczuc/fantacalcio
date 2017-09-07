b186d587a06bc735785caf204ab2c62b9a6ac241
zuk-8 <zuk-8@LAPTOP-MBCAT1HG.home>
2017-09-07 21:28:18 +0100

    ggfg: Added 2017

--- src/fantacalcio/scripts/get_grades_from_gazzetta.py
+++ src/fantacalcio/scripts/get_grades_from_gazzetta.py
@@ -1,9 +1,12 @@
 import os
-import getpass
 import pandas as pd
+from logging import Logger
 
 from scraping import get_team_soups, get_player_soup, get_player_stats
 from fantacalcio.config_data.config_data import FOLDER_FANTA_INPUT, FANTA_GRADES_FN
+
+log = Logger('get_grades_from_gazzetta')
+log.setLevel('INFO')
 
 
 # INPUTS
@@ -11,10 +14,18 @@ from fantacalcio.config_data.config_data import FOLDER_FANTA_INPUT, FANTA_GRADES
 # page_votes = "https://www.fantagazzetta.com/voti-serie-a/2016-17/"
 PAGES_GRADES = {'2014':"http://www.gazzetta.it/calcio/fantanews/voti/serie-a-2014-15/giornata-",
                 '2015':"http://www.gazzetta.it/calcio/fantanews/voti/serie-a-2015-16/giornata-",
-                '2016':"http://www.gazzetta.it/calcio/fantanews/voti/serie-a-2016-17/giornata-",}
-N_GIORNATE = 38
+                '2016':"http://www.gazzetta.it/calcio/fantanews/voti/serie-a-2016-17/giornata-",
+                '2017':"http://www.gazzetta.it/calcio/fantanews/voti/serie-a-2017-18/giornata-",
+}
+
+N_GIORNATE = {'2014':38,
+            '2015':38,
+            '2016':38,
+            '2017':2,
+}
+
 INDICES=['V', 'G', 'A', 'R', 'RS', 'AG', 'AM', 'ES', 'FV']
-OUTPUT_FOLDER = FOLDER_FANTA_INPUT[getpass.getuser()]
+OUTPUT_FOLDER = FOLDER_FANTA_INPUT[os.environ['USERNAME']]
 
 
 
@@ -23,8 +34,11 @@ OUTPUT_FOLDER = FOLDER_FANTA_INPUT[getpass.getuser()]
 def main():
     all_stats = pd.DataFrame()
     for season,page_grades in PAGES_GRADES.items():
+        print("\n"+"-"*160)
+        print("Loading season {s1}-{s2}".format(s1=season,s2=str(int(season)+1)))
         season_stats = pd.DataFrame()
-        for giornata in range(1,N_GIORNATE+1):
+        for giornata in range(1,N_GIORNATE[season]+1):
+            print("Season {}, giornata {}".format(str(season), str(giornata)))
             team_stats = pd.DataFrame()
             for team_soup in get_team_soups(page_grades, giornata):
                 team, player_soups = get_player_soup(team_soup)
@@ -42,7 +56,9 @@ def main():
         season_stats['Season'] = season
         all_stats = pd.concat([all_stats, season_stats])
     all_stats = all_stats.set_index(['Season', 'Giornata', 'Team', 'Player'])
+    log.info("Writing to csv")
     all_stats.to_csv(os.path.join(OUTPUT_FOLDER, FANTA_GRADES_FN))
+log.info("Finished script!")
 
 if __name__ == '__main__':
     main()
