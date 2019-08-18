e3dafa14a15eb2d60986e0a931f672a94ff104c9
Andrea Zuc <andrea.v.zucchelli@gmail.com>
2019-08-18 10:36:10 +0100

    updated player files

--- src/fantacalcio/scripts/get_player_links_from_gazzetta.py
+++ src/fantacalcio/scripts/get_player_links_from_gazzetta.py
@@ -8,29 +8,44 @@ from scraping import get_team_soups, get_player_soup, get_player_name, get_playe
 from fantacalcio.config_data.config_data import FOLDER_FANTA_INPUT, FANTA_LINKS_FN, FANTA_ATTRS_FN
 
 
-PAGES_GRADES = {'2014':"http://www.gazzetta.it/calcio/fantanews/voti/serie-a-2014-15/giornata-",
-                '2015':"http://www.gazzetta.it/calcio/fantanews/voti/serie-a-2015-16/giornata-",
-                '2016':"http://www.gazzetta.it/calcio/fantanews/voti/serie-a-2016-17/giornata-",}
+PAGES_GRADES = {
+    '2014':"http://www.gazzetta.it/calcio/fantanews/voti/serie-a-2014-15/giornata-",
+    '2015':"http://www.gazzetta.it/calcio/fantanews/voti/serie-a-2015-16/giornata-",
+    '2016':"http://www.gazzetta.it/calcio/fantanews/voti/serie-a-2016-17/giornata-",
+    '2017':"http://www.gazzetta.it/calcio/fantanews/voti/serie-a-2017-18/giornata-",
+    '2018':"http://www.gazzetta.it/calcio/fantanews/voti/serie-a-2018-19/giornata-",
+#     '2019':"http://www.gazzetta.it/calcio/fantanews/voti/serie-a-2019-20/giornata-",
+}
 N_GIORNATE = 38
 INDICES=['V', 'G', 'A', 'R', 'RS', 'AG', 'AM', 'ES', 'FV']
 OUTPUT_FOLDER = FOLDER_FANTA_INPUT[getpass.getuser()]
 
 
 
-def get_player_links_from_gazzetta():
+def store_player_links_from_gazzetta():
     # Get links in a dictionary
-    all_links = dict()
+    columns = ['Player', 'Season', 'Link']
+    all_links_df = pd.DataFrame(columns=columns)
     for season,page_grades in PAGES_GRADES.items():
+        all_links = pd.Series()
+        all_links.index.name = 'Player'
         for giornata in range(1,N_GIORNATE+1):
+            print("Fetching data for Season {s}, giornata {g}".\
+                  format(s=str(season), g=str(giornata)))
             for team_soup in get_team_soups(page_grades, giornata):
                 _, player_soups = get_player_soup(team_soup)
                 for player_soup in player_soups:
-                    all_links[(get_player_name(player_soup), season)] = get_player_link(player_soup)
-    # Move links to a DF
-    all_links_df = pd.DataFrame(columns=['Player', 'Season', 'Link'])
-    for k,v in all_links.items():
-        all_links_df = all_links_df.append(pd.DataFrame(data=[[k[0], k[1], v]], columns=['Player', 'Season', 'Link']))
-    all_links_df = all_links_df.set_index(['Season', 'Player']).sort_index().reset_index()
+                    player_name = get_player_name(player_soup)
+                    if player_name not in all_links.keys(): 
+                        all_links[player_name] = get_player_link(player_soup)
+            print("Storing data for Season {s} up to giornata {g}".\
+                  format(s=str(season), g=str(giornata)))
+            df = all_links.to_frame('Link').reset_index()
+            df['Season'] = season
+            all_links_df.append(df[columns]).to_csv(os.path.join(OUTPUT_FOLDER, FANTA_LINKS_FN), index=False)
+        all_links_df = all_links_df.append(df[columns])
+        print("Stored data for Season {s}\n".\
+              format(s=str(season)))
     return all_links_df
 
 def get_player_attributes_from_links(all_links_df):
@@ -53,9 +68,8 @@ def get_player_attributes_from_links(all_links_df):
     return player_attributes[columns]
 
 def main():
-    all_links_df = get_player_links_from_gazzetta()
-    all_links_df.to_csv(os.path.join(OUTPUT_FOLDER, FANTA_LINKS_FN), index=False)
-#     all_links_df = pd.read_csv(os.path.join(OUTPUT_FOLDER, FANTA_LINKS_FN))
+    all_links_df = store_player_links_from_gazzetta()
+#     all_links_df = pd.read_csv(os.path.join(OUTPUT_FOLDER, FANTA_LINKS_FN), encoding='latin-1')
     player_attributes = get_player_attributes_from_links(all_links_df)
     player_attributes.to_csv(os.path.join(OUTPUT_FOLDER, FANTA_ATTRS_FN), index=False)
 
